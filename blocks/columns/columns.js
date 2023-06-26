43e9c4d73545030c1c02d5cbf7c49cd51a90015c
Lance <lwintch@adobe.com>
2023-06-25 19:18:51 -0600

    incorporating Bryan's suggestions

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,25 +1,19 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
 export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
+  block.classList.add(`columns-${block.children.length}-cols`);
 
   // setup image columns
   [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
-    });
+    row.classList.add('column');
+    const decoration = row.children[0].textContent;
+    const content = row.children[1];
+    content.classList.add('content');
+    if (decoration) {
+      content.classList.add(decoration.replaceAll(/[^a-zA-Z-]/g, '-'));
+    }
+    row.replaceChildren(content);
   });
 
-  const icons = document.querySelectorAll(".columns.icon-steps.block .icon > svg");
-  icons.forEach(icon => {
-    icon.setAttribute('height', '43px');
-    icon.setAttribute('width', '43px');
-  })
-
+  decorateIcons(block);
 }
