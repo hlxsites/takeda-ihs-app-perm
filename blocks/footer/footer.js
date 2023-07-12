506873012047210718da828a60c5ae223b1bba0d
Lance Wintch <lwintch@users.noreply.github.com>
2023-07-12 11:58:43 -0600

    Feat/footer (#44)
    * added logo
    
    * feat(footer): adding the footer
    
    Adding the footer to the site as a custom block. This addresses issue #6.
    
    fix #6
    
    ---------
    
    Co-authored-by: Lance <lwintch@adobe.com>
    Co-authored-by: Bryan Stopp <bstopp@users.noreply.github.com>

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,26 +1,30 @@
-// import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // const cfg = readBlockConfig(block);
-  block.textContent = '';
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta).pathname : '/footer';
+  const resp = await fetch(
+    `${footerPath}.plain.html`,
+    window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {},
+  );
 
-  // fetch footer content
-  // const footerPath = cfg.footer || '/footer';
-  // const resp = await fetch(`${footerPath}.plain.html`,
-  // window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  if (resp.ok) {
+    const footer = document.createElement('div');
+    const html = await resp.text();
 
-  // if (resp.ok) {
-  //   const html = await resp.text();
-  //
-  //   // decorate footer DOM
-  //   const footer = document.createElement('div');
-  //   footer.innerHTML = html;
-  //
-  //   decorateIcons(footer);
-  //   block.append(footer);
-  // }
+    // decorate footer DOM
+    footer.innerHTML = html;
+
+    // size the footer image
+    const image = footer.querySelector('picture img');
+    image.setAttribute('width', '100');
+    image.removeAttribute('height');
+
+    decorateIcons(footer);
+    block.append(footer);
+  }
 }
